65721cc62f759073b99e00f7f38bbc241fc4addc
Norman Ziebal <s8nozieb@stud.uni-saarland.de>
2018-03-06 03:53:28 +0100

    Edited NavBar

--- reactjs/components/NavBar.jsx
+++ reactjs/components/NavBar.jsx
@@ -19,7 +19,9 @@ export default class NavBar extends React.Component {
             <Link to="/topics">Topics</Link>
           </NavItem>
           <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.1}>
+              <Link to="/topics">Topics</Link>
+            </MenuItem>
             <MenuItem eventKey={3.2}>Another action</MenuItem>
             <MenuItem eventKey={3.3}>Something else here</MenuItem>
             <MenuItem divider />
