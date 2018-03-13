6c4eb132d29081e596121f9c90bc9d756dce33e6
Norman Ziebal <s8nozieb@stud.uni-saarland.de>
2018-03-13 13:02:21 +0100

    added dynamic api call

--- reactjs/actions/apitestActions.js
+++ reactjs/actions/apitestActions.js
@@ -7,7 +7,7 @@ export const FETCH_API_TEST_ERROR500 = "FETCH_API_TEST_ERROR500"
 export const FETCH_API_TEST_FAILURE = "FETCH_API_TEST_FAILURE"
 export function fetchApiTest() {
   return function (dispatch) {
-    let url = "http://127.0.0.1:8000/api/v1/users/"
+    let url = process.BASE_API_URL+ "/users/"
     dispatch({type: FETCH_API_TEST})
     return request(
       url, {},
